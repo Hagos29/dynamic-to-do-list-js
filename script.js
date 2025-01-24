4712dd1effc2fb44310d6582de19f87c71af41d2
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-01-25 01:49:44 +0300

    Todo List project added

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // declare element by Id
+     addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const takeList = document.getElementById('task-list');
+
+    // addTask function
+    function addTask() {
+        const taskText = document.getElementById('task-input').ariaValueMax.trim();
+        if (taskText != " ") {
+            //add element
+            const li = document.createElement('li');
+            const addTask = document.createElement('span');
+            const newButton = document.createElement('span');
+             //add content
+             li.textContent = taskText;
+             newButton.textContent = 'Remove'
+            // add class
+            newButton.classList.add('remove-btn');
+            const newTask = document.getElementById('todo-app');
+            newTask.addEventListener('submit', function (e) {
+                e.preventDefault();
+                const value = newTask.querySelector('input[type="text]').value;
+                console.log(value);
+            })
+            // append to document
+            li.appendChild(newButton);
+            takeList.appendChild(li);
+            takeList.value = " ";
+        }; 
+        if (taskText == " ") {
+            alert.prompt('please enter a task');
+        }
+    }
+});
