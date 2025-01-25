473f9952d73763fd2647d36222912216e6a4ef87
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-01-25 13:05:36 +0300

    The todo list app  modified

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     // declare element by Id
-     const addButton = document.getElementById('add-task-btn');
+    const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const takeList = document.getElementById('task-list');
 
     // addTask function
     function addTask() {
-        const taskText = document.getElementById('task-input').ariaValueMax.trim();
+        const taskText = document.getElementById('task-input').value.trim();
         if (taskText != " ") {
             //add element
             const li = document.createElement('li');
-            const addTask = document.createElement('span');
+            li.textContent = taskText;
             const newButton = document.createElement('span');
-             //add content
-             li.textContent = taskText;
-             newButton.textContent = 'Remove'
-            // add class
+            newButton.textContent = 'Remove'
             newButton.classList.add('remove-btn');
-            const newTask = document.getElementById('todo-app');
-            newTask.addEventListener('submit', function (e) {
-                e.preventDefault();
-                const value = newTask.querySelector('input[type="text]').value;
-                console.log(value);
-            })
+
+            //const newTask = document.getElementById('todo-app');
+        newButton.onclick = function () {
+            takeList.removeChild(li);
+        }
             // append to document
             li.appendChild(newButton);
             takeList.appendChild(li);
             takeList.value = " ";
         }; 
         if (taskText == " ") {
-            alert.prompt('please enter a task');
+            alert('please enter a task');
         }
     }
   
